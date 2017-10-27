eef222638d99e73e2dcb97a4a42068fc385ae938
Drew <sombersunday@gmail.com>
2017-10-27 10:12:14 -0400

    Title Comment

--- PolyMorphism-Exercise1/Program.cs
+++ PolyMorphism-Exercise1/Program.cs
@@ -10,6 +10,10 @@ namespace PolyMorphism_Exercise1
     {
         static void Main(string[] args)
         {
+            //Exercises for Section 5, Lecture 31 of C# Intermediate Classes
+            //https://www.udemy.com/csharp-intermediate-classes-interfaces-and-oop/learn/v4/t/lecture/2480692?start=0
+            //Polymorphism Exercise 1
+
             Console.WriteLine("Display Examples of class implementation from Abstract Class and Method");
 
             //var sqlError = new SqlConnection(null); //Test Exception Error
